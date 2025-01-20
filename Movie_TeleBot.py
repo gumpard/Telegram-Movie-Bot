cedfeec51bc878b217a7e3df7492c5f26478baa5
gumpard <142250820+gumpard@users.noreply.github.com>
2025-01-20 10:40:07 +0100

    Update Movie_TeleBot.py
    Tokens entfernt

--- Movie_TeleBot.py
+++ Movie_TeleBot.py
@@ -9,7 +9,7 @@ import requests
 import random
 
 #create telebot 
-bot = telebot.TeleBot(token = "7833129925:AAHW88bb-svt9_2oabehcapnlDF4gTp-_GU")
+bot = telebot.TeleBot(token = "<YOUR_TELEBOT_TOKEN>")
 
 #global variables
 global category, searchType, genre_auswahl,chatID
@@ -18,7 +18,7 @@ items = []
 genres = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]
 
 # TMDb-Funktionen ================================================================================================
-tmdb.API_KEY = "b0c4a4e00c846e0322bbfea2f054f5a9"
+tmdb.API_KEY = "<YOUR_TMDB_TOKEN>"
 
 def discover_tmdb(media_type, genre,pages):
     discover = tmdb.Discover()
